359c465a0b86491c4832d68d0dbf7b7898d581e9
Verda Korzeniewski <verdakor@gmail.com>
2024-05-15 20:12:47 -0400

    Initial Commit

---
+++ The Social Running App/The_Social_Running_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Social_Running_AppApp.swift
+//  The Social Running App
+//
+//  Created by Verda Korzeniewski on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct The_Social_Running_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
